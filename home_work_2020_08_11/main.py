39e476d2991edbbca865f14d0964d7a0d6e49577
pozariv <artem.pozdeev.uz@gmail.com>
2020-08-13 23:25:43 +0500

    done

--- home_work_2020_08_11/main.py
+++ home_work_2020_08_11/main.py
@@ -1,11 +1,37 @@
 import operator
+import datetime
+from functools import wraps
 
 # common variables
 n = 10
 list_numbers = [i for i in range(0, n)]
 
 
+# decorators
+def count_time(func):
+    @wraps(func)
+    def wrapper(arg1, arg2):
+        start_time = datetime.datetime.now()
+        res = func(arg1, arg2)
+        end_time = datetime.datetime.now()
+        time_spent = end_time - start_time
+        print('time spent for:', func.__name__, time_spent)
+        return res
+    return wrapper
+
+
+def count_iterations(func):
+    @wraps(func)
+    def wrapper(arg1, arg2, arg3):
+        print('-' * arg3, '>', arg3)
+        res = func(arg1, arg2, arg3)
+        print('-' * arg3, '>', arg3)
+        return res
+    return wrapper
+
+
 # task #1
+@count_time
 def calculate_power_for_collection(input_list, pow_number=2):
     power_list = list_numbers.copy()
     for i in power_list:
@@ -16,85 +42,67 @@ def calculate_power_for_collection(input_list, pow_number=2):
     return output_list
 
 
-# pow_number = 2
-# powered_list = calculate_power_for_collection(list_numbers, pow_number)
-# print('task #1')
-# print('source list:', list_numbers)
-# print('powered in', pow_number)
-# print('final list:', powered_list)
-# print('')
+print('task #1')
+pow_number = 2
+powered_list = calculate_power_for_collection(list_numbers, pow_number)
+print('source list:', list_numbers)
+print('powered in', pow_number)
+print('final list:', powered_list)
+print('')
 
 
 # task #2
+@count_time
 def get_filtered_numbers(input_list, numbers_type):
 
-    def get_even(x):
-        if x % 2 == 0:
-            return x
-
-    def get_odd(x):
-        if x % 2 != 0:
-            return x
-
     def get_prime(x):
+        if x == 1:
+            return True
         temper: int = 2
         while x % temper != 0:
             temper += 1
-            if temper == x:
-                break
-        return x
+        return temper == x
 
     if numbers_type == 'even':
-        return list(filter(get_even, input_list))
+        return list(filter(lambda x: x % 2 == 0, input_list))
     elif numbers_type == 'odd':
-        return list(filter(get_odd, input_list))
+        return list(filter(lambda x: x % 2 != 0, input_list))
     elif numbers_type == 'prime':
         return list(filter(get_prime, input_list))
 
 
-
-# NUMBERS_TYPE = 'prime'  # 'even/odd/prime'
-# filtered_list = get_filtered_numbers(list_numbers, NUMBERS_TYPE)
-# print('task #2')
-# print('source list:', list_numbers)
-# print('filtered to get', NUMBERS_TYPE, 'numbers')
-# print('final list:', filtered_list)
-# print('')
+print('task #2')
+NUMBERS_TYPE = 'even'  # 'even/odd/prime'
+filtered_list = get_filtered_numbers(list_numbers, NUMBERS_TYPE)
+print('source list:', list_numbers)
+print('filtered to get', NUMBERS_TYPE, 'numbers')
+print('final list:', filtered_list)
+print('')
 
 
 # task #3
-def apend_next_phi(collect, max_count=20, a=0, b=1, counter=0):
+@count_iterations
+def apend_next_phi(collect, max_count=20, counter=0):
     """
     :param collect: list of numbers phi
-    :param a: first phi number
-    :param b: second phi number
-    :param counter: counting calculated numbers
+     :param counter: counting calculated numbers
     :param max_count: contains maximum iterations
     :return: collect - list with appended phi number
     """
 
-    c = a + b
-    collect.append(c)
+    collect.append(collect[-1] + collect[-2])
     counter += 1
     if max_count == counter:
         return collect
     else:
-        apend_next_phi(collect, max_count, b, c, counter)
+        apend_next_phi(collect, max_count, counter)
     return collect
 
-
+print('task #3')
 coll = [0, 1]
 maxcount = 15
-res_phi = apend_next_phi(coll, maxcount)
-
-# print('task #3')
-# print('calculating', maxcount, 'items of phi numbers')
-# print('phi list', end=": ")
-# for i in res_phi:
-#     print(i, end=", ")
-
-n = 4
-d = 2
-while n % d != 0:
-    d += 1
-print(d == n)
+res_phi = apend_next_phi(coll, 15, 0)
+print('calculating', maxcount, 'items of phi numbers')
+print('phi list', end=": ")
+for i in res_phi:
+    print(i, end=", ")
